090418f7b3ee9cbe31eac48cbd51f155078ae94d
Aitishnitsa <aitishnitsa@gmail.com>
2025-05-31 22:42:43 +0300

    hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import AdminPanel from './pages/AdminPanel';
 import Auth from './pages/Auth';
 import Profile from './pages/Profile';
@@ -8,17 +8,16 @@ import Reader from "./pages/Reader.jsx";
 function App() {
   return (
     <>
-      <BrowserRouter basename="/ebook/">
+      <HashRouter>
         <Nav />
         <Routes>
-          <Route path="/" element={<Navigate to="/admin" />} />
-          <Route path="/admin" element={<AdminPanel />} />
+          <Route path="/" element={<AdminPanel />} />
           <Route path="/auth" element={<Auth />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/reader" element={<Reader />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
